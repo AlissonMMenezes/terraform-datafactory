cc83c6de649b343b91861e4b605a9e52deaf2940
Alisson Menezes <Alisson.itops@gmail.com>
2022-03-05 17:12:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "dl" {
   resource_group_name   = var.resource_group_name
   data_factory_id       = azurerm_data_factory.df.id
   use_managed_identity  = true
-  tenant                = var.tenantid
+  tenant                = "8abcf116-35fa-47ac-90ff-0d9db900a1a4"
   url                   = data.azurerm_storage_account.sa.primary_dfs_endpoint
 }
 
